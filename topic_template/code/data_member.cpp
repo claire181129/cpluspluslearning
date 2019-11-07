1c6e5e1d47e1995450eaff7a0959f462b1ff5aa8
claire181129 <jtq0521@163.com>
2019-11-07 13:45:27 +0800

    create new branch 'template'

---
+++ topic_template/code/data_member.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+struct A{
+	std::string s = "I'm A";
+}
+
+struct B
+
+struct CPU{
+	std::string cpu_arch;//'x86_64','IA-64', 'AArch64'
+	TYPE cpu;
+}
+
+int main(int argc,char* argv[])
+{
+	//detect cpu architecture type
+	CPU<>  data;
+}
